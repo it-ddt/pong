1db175d97d9502ee1458d5e31938dc6a62e3ad23
it-ddt <45958272+it-ddt@users.noreply.github.com>
2023-05-16 17:44:15 +0300

    Add files via upload

--- main_1gr_oop.py
+++ main_1gr_oop.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-from random import randint
+from random import randint, choice
+from math import radians, sin, cos
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -29,9 +30,13 @@ class Game:
             (self.screen_rect.width * 0.9, self.screen_rect.centery),
             is_automatic=True,
         )
+        ball = Ball(self.screen_rect)
+        ball.throw_in()
         self.paddles = pygame.sprite.Group()
+        self.ball = pygame.sprite.Group()
         self.paddles.add(player_1)
         self.paddles.add(player_2)
+        self.ball.add(ball)
         self.clock = pygame.time.Clock()
         self.main_loop()
         
@@ -47,8 +52,10 @@ class Game:
                 game = False
 
             self.paddles.update()
+            self.ball.update()
             self.screen.fill(BLACK)
             self.paddles.draw(self.screen)
+            self.ball.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(FPS)
         pygame.quit()
@@ -95,22 +102,56 @@ class Paddle(pygame.sprite.Sprite):
 
 
 class Ball(pygame.sprite.Sprite):
-    """ мяч """
+    """
+    мяч
+    отскок от бортов (верхнего и нижнего)
+    отскок от ракеток
+    """
     def __init__(
             self,
             screen_rect=None,
-            center=(0, 0),
+            center=None,
             color=WHITE,
             size=None,
             speed=10,
+            direction=90,
+            vel_x=0,
+            vel_y=0,
     ):
         super().__init__()
         self.screen_rect = screen_rect
         if not size:
-            size = (self.screen_rect.width * 0.01, self.screen_rect.width * 0.1)
+            size = (self.screen_rect.width * 0.01, self.screen_rect.width * 0.01)
         self.image = pygame.Surface(size)
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        if not center:
+            self.rect.center = screen_rect.center
+        self.speed = speed
+        self.direction = direction
+        self.vel_x = vel_x
+        self.vel_y = vel_y
 
-        
+    def update(self):
+        self.vel_x = sin(radians(self.direction)) * self.speed
+        self.vel_y = cos(radians(self.direction)) * self.speed * -1
+        self.rect.x += self.vel_x
+        self.rect.y += self.vel_y
+        self.bounce()
+
+    def throw_in(self):
+        self.rect.center = self.screen_rect.center
+        self.direction = choice((randint(45, 135), randint(225, 315)))
+
+    def bounce(self):
+        if self.rect.top <= self.screen_rect.top:
+            self.direction *= -1
+            self.direction += 180
+        elif self.rect.bottom >= self.screen_rect.bottom:
+            self.direction *= -1
+            self.direction += 180
+
+
 class Score:
     """ табло """
     pass
